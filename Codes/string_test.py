07bfc38f87853759571a603051ed8752a24bdabd
shohan4556 <shohan4556@gmail.com>
2019-10-12 22:21:06 +0600

    string of python

--- Codes/string_test.py
+++ Codes/string_test.py
@@ -1,7 +1,33 @@
-greeting = "G'day mate"
+greeting = "G'day-mate."
 name = "shohan"
 description = "Happy new year "
 year = 2020
 sentence = description + str(year) + " " +name 
 
 print(sentence)
+
+movie1 = "\n\tAkira"
+movie2 = "\n\tThe Necessary Death of Charlie Countryman"
+
+print("My Favorite Movies: "+movie1+movie2)
+
+#library function 
+print(type(movie1))
+
+#library function
+print(len(sentence))
+
+#substring print 
+print(name[1:4])
+
+#middle character of greeting 
+#integer division 
+mid = len(greeting)//2 
+print(greeting[mid])
+
+
+word = 'Python'
+first = word[0]
+rest = word[1:]
+result = rest + '-'+first+'y'
+print(result)
