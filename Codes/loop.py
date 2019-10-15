fd62786bfe52bf96e8870a387cfecb0167caa7d8
Shohan <shohan4556@gmail.com>
2019-10-15 13:48:31 +0600

    loop test

---
+++ Codes/loop.py
@@ -0,0 +1,21 @@
+import random as rnd 
+
+price = [20,50,10,30,99,1,2,3]
+
+#loop through the list 
+for i in price:
+    print(i)
+
+print(rnd.random())
+
+#loop 10 times 
+for i in range(10):
+    print(i)
+
+
+#loop through a list 
+for i in range(2,10,2):
+    print(i)
+
+#random integer between a range 
+print(rnd.randint(1,50))
