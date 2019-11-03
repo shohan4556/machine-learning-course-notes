ba9299f6f4e2ad65f93d6362b3f527325382a21d
Shohan <shohan4556@gmail.com>
2019-11-03 13:57:11 +0600

    plot decoration

--- Codes/Matplotlib/lineplot.py
+++ Codes/Matplotlib/lineplot.py
@@ -2,8 +2,15 @@ import matplotlib.pyplot as plt
 
 year = [10,20,30,40,50]
 pop = [1,2,3,4,5]
+
 # Make a line plot: year on the x-axis, pop on the y-axis
 plt.plot(year,pop)
+
+plt.xlabel("year")
+plt.ylabel("population")
+plt.title("world population")
+plt.yticks([2,4,6,8,10],
+          ['0B','2B','4B','6B','8B'])
 
 # Display the plot with plt.show()
 plt.show()
