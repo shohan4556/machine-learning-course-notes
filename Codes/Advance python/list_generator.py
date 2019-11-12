157ee9544aa3d7787a3b848c52b206b686678ee1
Shohan <shohan4556@gmail.com>
2019-11-12 16:51:40 +0600

    list comprehension and generator

---
+++ Codes/Advance python/list_generator.py
@@ -0,0 +1,27 @@
+# list comprehension creates an entire list 
+# generator does not creates list instead creates generator objects 
+
+# Create list comprehension: squares
+squares = [i **2 for i in range(0,10)]
+print(squares)
+
+# Create a 5 x 5 matrix using a list of lists: matrix
+matrix = [[col for col in range(5)] for row in range(5)]
+print(matrix)
+
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+print(new_fellowship)
+
+
+# Create a list of strings: lannister
+lannister = ['cersei', 'jaime', 'tywin', 'tyrion', 'joffrey']
+
+# Create a generator object: lengths
+lengths =(len(person) for person in lannister)
+
+# Iterate over and print the values in lengths
+for value in lengths:
+    print(value)
