9aac059b15b7a01fcc28458c6dbe1652d628fcbd
Shohan <shohan4556@gmail.com>
2019-12-03 13:39:19 +0600

    KNN update

--- Codes/Algo Implement/KNN.py
+++ Codes/Algo Implement/KNN.py
@@ -1,2 +1,26 @@
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as pd 
+
+# import dataset from csv as dataframe  
+# each column is a features and each rows are observation or data points 
+# target needs to a single column with the same number of observation as feature data 
+
+"""
+#target/response variable 
+y = df['party'].values
+
+#features array 
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit/train the classifier to the data
+knn.fit(X,y)
+
+# then predict for new data
+
+"""
+
+
 
