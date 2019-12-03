5080901afc26d5947cf4c0753b23d346c6fdcb45
shohan4556 <shohan4556@gmail.com>
2019-12-03 23:48:25 +0600

    updated

--- Codes/Algo Implement/KNN.py
+++ Codes/Algo Implement/KNN.py
@@ -1,26 +1,35 @@
 from sklearn.neighbors import KNeighborsClassifier
-import numpy as pd 
+import pandas as pd 
 
 # import dataset from csv as dataframe  
 # each column is a features and each rows are observation or data points 
 # target needs to a single column with the same number of observation as feature data 
 
+
+df = pd.read_csv('Codes/Algo Implement/gm_2008_region.csv', index_col=0)
+# rename columns
+print(df.head())
+
+
+
 """
-#target/response variable 
+# Create arrays for the features and the response variable
 y = df['party'].values
-
-#features array 
 X = df.drop('party', axis=1).values
+#print(X)
 
-# Create a k-NN classifier with 6 neighbors
+# Create a k-NN classifier with 6 neighbors: knn
 knn = KNeighborsClassifier(n_neighbors=6)
 
-# Fit/train the classifier to the data
-knn.fit(X,y)
+# Fit the classifier to the data
+knn.fit(X, y)
 
-# then predict for new data
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+
 # Predict and print the label for the new data point X_new
 new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
 
 """
 
