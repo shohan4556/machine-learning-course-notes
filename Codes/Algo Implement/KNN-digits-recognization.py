10900be83bcf3ffba65a0e1d4988eceb8be41498
shohan4556 <shohan4556@gmail.com>
2019-12-04 23:12:58 +0600

    minor changes

--- Codes/Algo Implement/KNN-digits-recognization.py
+++ Codes/Algo Implement/KNN-digits-recognization.py
@@ -1,4 +1,3 @@
-# Import necessary modules
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
@@ -11,14 +10,14 @@ import numpy as np
 digits = datasets.load_digits()
 
 # Print the keys and descriptions of the dataset
-print(digits.keys())
-print(digits['DESCR'])
+# print(digits.keys())
+# print(digits['DESCR'])
 
 # Print the shape of the images and data keys
 print(digits.images.shape)
 print(digits.data.shape)
 
-# Display digit 1010
+# Display digit 100
 # display image 
 plt.imshow(digits.images[100], cmap=plt.cm.gray_r, interpolation='nearest')
 plt.show()
@@ -28,8 +27,6 @@ plt.show()
 # Create feature and target arrays
 X = digits.data # 8x8 pixel image each pixel representes integer 0 -> 16 
 y = digits.target # 0 to 9 digits
-
-print(y)
 
 # Split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
