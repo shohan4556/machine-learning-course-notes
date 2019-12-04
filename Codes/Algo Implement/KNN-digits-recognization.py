f848c1caa4778ebf5e0e5446e4175efc5ed94103
Shohan <shohan4556@gmail.com>
2019-12-04 14:02:08 +0600

    plotting KNN accuracy for different neighbr value

--- Codes/Algo Implement/KNN-digits-recognization.py
+++ Codes/Algo Implement/KNN-digits-recognization.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 #####----------EDA---------------#####
 # Load the digits dataset: digits
@@ -41,4 +42,38 @@ knn.fit(X_train, y_train)
 
 # Print the accuracy
 print(knn.score(X_test, y_test))
+
+
+#####----------Accuracy Plotting---------------#####
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train, y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+    
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
 
