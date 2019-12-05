9b9a50b35c9e454cb9a0811fcb72679431345ad9
Shohan <shohan4556@gmail.com>
2019-12-05 17:13:48 +0600

    linear regression using scikit learn

---
+++ Codes/Algo Implement/Linear-Regression.py
@@ -0,0 +1,25 @@
+"""
+from sklearn.linear_model import LinearRegression 
+
+# Create the regressor: reg
+reg = LinearRegression()
+
+# Create the prediction space
+prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1,1)
+
+# Fit the model to the data
+# X = feature -> fertility 
+# y = target -> life
+# todo import the value form csv reshape and plot 
+reg.fit(X_fertility, y)
+
+# Compute predictions over the prediction space: y_pred
+y_pred = reg.predict(prediction_space)
+
+# Print R^2 
+print(reg.score(X_fertility, y))
+
+# Plot regression line
+plt.plot(prediction_space, y_pred, color='black', linewidth=3)
+plt.show()
+"""
