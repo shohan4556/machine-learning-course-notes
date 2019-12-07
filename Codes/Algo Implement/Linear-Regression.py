2a26c23794efa67ced5f13c07d870a12e45ff9b1
shohan4556 <shohan4556@gmail.com>
2019-12-07 21:34:23 +0600

    linear regression

--- Codes/Algo Implement/Linear-Regression.py
+++ Codes/Algo Implement/Linear-Regression.py
@@ -1,42 +1,46 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-# Create the regressor: reg
-reg = LinearRegression()
+# load data 
 df = pd.read_csv('Codes/Algo Implement/gm_2008_region.csv', index_col=0)
-#print(df.head)
 
 X_fertility = df['fertility'].values
 y_life = df['life'].values
 
+# reshape for scikit learn 
 X_fertility = X_fertility.reshape(-1, 1)
 y_life = y_life.reshape(-1,1)
 
-#print(X_fertility.shape)
+# create regressor 
+reg = LinearRegression()
 
-#Create the prediction space
-prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1,1)
+# test and train split 
+X_train, X_test, y_train, y_test = train_test_split(X_fertility, y_life, test_size = 0.3, random_state = 42)
 
 # Fit the model to the data
 # X = feature -> fertility 
 # y = target -> life
 
-reg.fit(X_fertility, y_life)
+reg.fit(X_train, y_train)
 
-# feature -> prediction space 
-# target -> y_pred
-y_pred = reg.predict(prediction_space)
-print(y_pred)
+# predict with test data 
+y_pred = reg.predict(X_test)
 
 # Print R^2 
 print(reg.score(X_fertility, y_life))
+
+# print root mean square error of cost function 
+# average squared difference between the estimated values and the actual value
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error : ', rmse)
 
 # Plot regression line
 plt.scatter(df['fertility'].values, df['life'].values)
 plt.xlabel('Fertility')
 plt.ylabel("Life Expectancy")
-
-plt.plot(prediction_space, y_pred, color='black', linewidth=3)
+plt.plot(X_test, y_pred, color='black', linewidth=3)
 plt.show()
