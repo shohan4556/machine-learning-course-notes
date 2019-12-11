a1f811c1fd5c8a41a2d5e0a54af869e018313059
shohan4556 <shohan4556@gmail.com>
2019-12-11 23:08:29 +0600

    logistic regression

--- Codes/Algo Implement/Logistic_Regression.py
+++ Codes/Algo Implement/Logistic_Regression.py
@@ -1,17 +1,25 @@
 from sklearn.linear_model import LogisticRegression 
 from sklearn.metrics import confusion_matrix,classification_report 
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 from sklearn import datasets
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-'''
-# Create training and test sets
+
+df = pd.read_csv('Codes/Algo Implement/diabetes.csv')
+X = df.drop(columns='diabetes')
+y = df['diabetes']
+
+#y = y.reshape(-1,1)
+
+#print(y)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
 
-# Create the classifier: logreg
-logreg = LogisticRegression()
+logreg = LogisticRegression(solver='lbfgs')
 
 # Fit the classifier to the training data
 logreg.fit(X_train, y_train)
@@ -20,6 +28,21 @@ logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 
 # Compute and print the confusion matrix and classification report
-print(confusion_matrix(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred).ravel())
 print(classification_report(y_test, y_pred))
+
+'''
+
+# Compute predicted probabilities: y_pred_prob
+y_pred_prob = logreg.predict_proba(X_test)[:,1]
+
+# Compute and print AUC score
+print("AUC: {}".format(roc_auc_score(y_test,y_pred_prob)))
+
+# Compute cross-validated AUC scores: cv_auc
+cv_auc = cross_val_score(logreg, X,y, cv=5, scoring='roc_auc')
+
+# Print list of AUC scores
+print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
+
 '''
