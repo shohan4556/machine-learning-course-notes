6145a8cce69f0a53ff3f3332623fc26a136fcc5d
Shohan <shohan4556@gmail.com>
2019-12-17 12:57:34 +0600

    preprocessing missing value

--- Codes/Algo Implement/Preprocess-data.py
+++ Codes/Algo Implement/Preprocess-data.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import numpy as np 
 
 df = pd.read_csv('Codes/Algo Implement/gm_2008_region.csv', index_col=0)
 
@@ -11,4 +12,16 @@ print(df_region.columns)
 
 df.boxplot('life', 'Region', rot=60)
 plt.show()
+
+
+# drop row of missing value 
+voteDF = pd.read_csv('Codes/Algo Implement/house-votes-84.csv', index_col=0)
+# replace missing value by Nan
+voteDF[voteDF == '?'] = np.nan
+
+print(voteDF.isnull().sum())
+# drop all the rows of missing value 
+voteDF = voteDF.dropna()
+
+print(voteDF.head())
 
