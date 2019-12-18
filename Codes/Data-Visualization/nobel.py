129208c465b9814d443d1844019baee50f57401c
shohan4556 <shohan4556@gmail.com>
2019-12-18 22:46:58 +0600

    nobel EDA

---
+++ Codes/Data-Visualization/nobel.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+
+nobel  = pd.read_csv('Codes/Data-Visualization/nobel.csv')
+print(nobel.head(10))
+
