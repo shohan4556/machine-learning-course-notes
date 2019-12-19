9e455ea28eb368060e5592b4a033ff6983f3816e
Shohan <shohan4556@gmail.com>
2019-12-19 15:37:51 +0600

    nobel prize data visualize

--- Codes/Data-Visualization/nobel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
-
-
-nobel  = pd.read_csv('Codes/Data-Visualization/nobel.csv')
-print(nobel.head(10))
-
--- Codes/spyder-project/spyder-project/nobel.py
+++ Codes/spyder-project/spyder-project/nobel.py
@@ -12,6 +12,23 @@ print(nobel.shape)
 print(nobel.columns)
 print(nobel.head(10))
 
+plt.figure()
+sns.countplot(x='Sex', hue='Sex',data=nobel)
+plt.title('Nobel prize won by Male and Female from 1901 to 2016')
+plt.savefig('male-vs-female.png')
 
-ax = sns.countplot(x='Sex', hue='Sex',data=nobel)
-ax.set_title('Nobel prize won by Male and Female from 1901 to 2016')
+category = nobel['Category'].value_counts()
+#print(category)
+
+plt.figure()
+sns.barplot(x=category.index, y=category.values)
+plt.title('Nobel Prize Category')
+plt.savefig('category.png')
+
+country = nobel['Birth Country'].value_counts().head(10)
+plt.figure()
+sns.barplot(x=country.index, y=country.values)
+plt.title('Top 10 Nobel Prize Winner Country')
+plt.xticks(rotation=90)
+plt.savefig('country.pdf',  bbox_inches = "tight")
+
